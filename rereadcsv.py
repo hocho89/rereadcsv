dad8cb3bc89573792be7b0a0304c70f03c3edf59
hocho89 <h.engstrom123@gmail.com>
2020-08-13 16:30:36 +0200

    added input file path and output command print, still issue with 2xquotes

--- rereadcsv.py
+++ rereadcsv.py
@@ -1,28 +1,44 @@
 import csv
 import operator
 import re
+from pathlib import Path, PureWindowsPath
+import ast
+import shlex
+
+#filepath = input("ange sökväg till csv: ")
+#filename = Path(filepath) #input file path
+#path_on_windows = PureWindowsPath(filename) #make file path python compatible eg / instead of \
+
+
 
 with open("C:/Users/Loco/Desktop/Git project/rereadcsv/Data/rereaddata.csv") as infile:
-    reader = csv.reader(infile) # Create a new reader
+    reader = csv.reader(infile, delimiter = ",") #Create a new reader
     next(reader) # Skip the first row
+    data = []
     data = list(reader)
-    flexNumberList = [row[3].split(",")[0] for row in data] #splits the out every value in col 3 from data list
-    serverNameList = [row[0].split(",")[0] for row in data] #splits the out every value in col 1 from data list
+    #for data1 in reader:
+        #data1 = [d.replace('"', ',') for d in data1]
+       # data1 = list(data1)
+        #print("lol", data1)
+
+flexNumberList = [row[3].split(",")[0] for row in data] #splits the out every value in col 3 from data list
+serverNameList = [row[1].split(",")[0] for row in data] #splits the out every value in col 1 from data list
     
 tempList = flexNumberList
 
 flexNumberList = list(map(int, flexNumberList)) # convert list from string to int
-flexNumberList.sort(reverse=False) # sort biggest first
+flexNumberList.sort(reverse=False) # sort biggest first in list
 
 tempList = list(map(int, tempList)) # convert list from string to int
-tempList.sort(reverse=False) # sort biggest first
+tempList.sort(reverse=False) # sort biggest first in list
 
 tempList = ([j-i for i, j in zip(flexNumberList[:-1], flexNumberList[1:])]) # count the diff between next and prev item
 
-for b, c in zip (flexNumberList, tempList): # print every item in both list, zip to iterate both lists in same print
-    print (b,c)
+#for b, c in zip (flexNumberList, tempList): # print every item in both list, zip to iterate both lists in same print
+   #print (b,c)
 
 tempInt = 0
+
 for i in tempList: 
     if(i >= 2) : 
         fixedFlexNumbers = []
@@ -30,63 +46,21 @@ for i in tempList:
         fixedFlexNumbers.append((flexNumberList[index] if index < len(flexNumberList) else default)) #add the value of the input index
         tempInt = ((flexNumberList[index] if index < len(flexNumberList) else default))
         countFlexDiff = []
-        testTemp = 0
+        testString = ""
         while True:
             countFlexDiff.append(1)
             total = sum(countFlexDiff)
             if total == i:
                 break
-        print(*countFlexDiff, sep = "\n")
-        print("values to diff iterate: ", tempInt)
+        #print(*countFlexDiff, sep = "\n")
         for b in countFlexDiff:
             tempInt -= 1
-            print(tempInt)
+            testString +=  str(tempInt) + "|"
+            #print("egrep \|(",tempInt,"|flexnumber)\| SYSTEMNAMN_DATUM.log",tempInt)
+            
+        print('egrep "\|('+testString+')\|"'+ serverNameList[0])
 
             
 
-
-
-
-
-
 print("Checking if 4 exists in list ( using in ) : ") 
 
-
-
-# 4/5 done
-# list med de nummer som är mer än 2 + nästa nummer 
-    #iterara varje nummer mellan aktuellt numer och nästa nummer
-# if templist[i]>=2:
-# flexNumberList [i] - tempList [i] 
-
-
-
-#for i in tempList: print(i)
-
-#count = 0 4/5
-#for p in flexNumberList: 
-    #tempList[count] = flexNumberList[count]
-    #tempList.remove[count]
-    #count +=1
-    
-
-#months.append(int(reversed))
-#sort_orders = sorted(months.items(), key=lambda x: x[1])
-
-#for i in months:
-#	print(months[i+1])
-
-
-
-#print(months)
-
-#print(months)
-
-#reader = csv.DictReader(open('C:/Users/Loco/Desktop/Git project/rereadcsv/Data/rereaddata.csv', 'r'))
-#result = sorted(reader, key=lambda d: float(d['appnumber']))
-
-#data = []
-#for line in result:
- #    months = [row[line].split(",")[line] for row in result]
-
-#print(months)
