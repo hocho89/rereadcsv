592d3aace827e6f8d37875ed8a47dca803b5fffd
hocho89 <h.engstrom123@gmail.com>
2020-09-16 21:07:54 +0200

    added shit

--- ReadMultipleFiles.py
+++ ReadMultipleFiles.py
@@ -6,37 +6,40 @@ from datetime import datetime
 from os import listdir
 from os.path import isfile, join
 
-
-
-
 def read_directory():
     filepath_list = []   
     filepath = input("ange sökväg till mapp: ")
     filename = Path(filepath) #input file path
     path_on_windows = PureWindowsPath(filename)
-    onlyfiles = [f for f in listdir(path_on_windows) if isfile(join(path_on_windows, f))]
-    filepath_list = ([str(filepath) + s for s in onlyfiles])
-    print(filepath_list[1:4])
-        
+    onlyfiles = [f for f in listdir(path_on_windows) if isfile(os.path.join(path_on_windows, f))]
+    filepath_list = (([filepath+ "\\" + s  for s in onlyfiles]))
+    counter = 0
+    global data
+    data = []
+    for i in filepath_list:
+        pre_init_csvfile(filepath_list[counter])
+        data += init_csvfile(filepath_list[counter])
+        counter += 1
+    return data
 
-def get_filepath():
-    filepath = input("ange sökväg till csv: ")
-    filename = Path(filepath) #input file path
-    path_on_windows = PureWindowsPath(filename) #make file path python compatible eg / instead of \
-    #init_csvfile(path_on_windows)
-    return path_on_windows
+def pre_init_csvfile(path_on_windows):
+    with open(path_on_windows, 'a', newline='', encoding="utf-8") as csv_file:
+        writer=csv.writer(csv_file)
+        writer.writerow(['2020/07/15 00:13:37 CEST','KUKSYSTEM','lx555533.dd.pppppp.ssss','0'])
+        csv_file.close()
 
 def init_csvfile(path_on_windows):
+    data = []
     with open(path_on_windows, "r+", encoding="utf-8") as csv_file:
         content = csv_file.read()
 
-    with open(path_on_windows, "w+", encoding="utf-8") as csv_file:
+    with open(path_on_windows, "w+", newline='',encoding="utf-8") as csv_file:
         csv_file.write(content.replace('"', ''))
 
     with open(path_on_windows) as infile:
         reader = csv.reader(infile, delimiter = ",") #Create a new reader
         next(reader) # Skip the first row
-        data = list(reader)
+        data += list(reader)
         csv_file.close()
     return data
 
@@ -49,9 +52,10 @@ def split_values_from_csv(data, what_list):
     return {
         'flist': fnumber_list,
         'servname': servername_list,
-        'datelist': date_list,
+        'format_datelist': format_datelist(date_list),
         'systemname': system_name,
-        'datelist': date_list
+        'datelist': date_list,
+        #'sorted_list': sort_list(fnumber_list)
     }.get(what_list, "not found")
 
 def format_datelist(date_list):
@@ -60,17 +64,30 @@ def format_datelist(date_list):
         j = b.replace(' ', '-')
         j = b.replace("/","-")
         date_list_temp.append(j)
-    date_string = str(date_list_temp[0]) #converted one date row to string
+    date_string = str(date_list_temp) #converted one date row to string
     return date_string
 
-
 def sort_list(csv_list):
-    fNumberList = list(map(int, fNumberList)) # convert list from string to int
-    fNumberList.sort(reverse=False) # sort biggest first in list
-    fNumberList.pop(1)
+    csv_list = list(map(int, csv_list)) # convert list from string to int
+    csv_list.sort(reverse=True) # sort biggest first in list
+    return csv_list
+
+def calculate_difference_in_list(csv_list):
+    csv_list = list(map(int, csv_list)) 
+    csv_list = ([j-i for i, j in zip(csv_list[:-1], csv_list[1:])])
+    return csv_list
+
+def create_list_per_system(flist, system_name_list, flist_diff):
+    flist = list(map(int, flist)) 
+    flist_diff = list(map(int, flist_diff)) 
+    for item_flist, item_system_name, item_flist_diff in zip(flist, system_name_list, flist_diff):
+        print(item_flist, item_system_name, item_flist_diff, (item_flist-item_flist_diff))
 
 
-#init sequence
-#init_csvfile(get_filepath()) #data 
-#print(split_values_from_csv(init_csvfile(get_filepath()), 'flist'))
 read_directory()
+#print(calculate_difference_in_list(split_values_from_csv(data, 'flist')))
+#print(split_values_from_csv(data, 'systemname'))
+
+
+
+create_list_per_system(split_values_from_csv(data, 'flist'),split_values_from_csv(data, 'systemname'),calculate_difference_in_list(split_values_from_csv(data, 'flist')))
